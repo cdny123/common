bec04798791c6ca77222cc7130629a042acea597
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-01-17 14:07:51 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -129,20 +129,11 @@ LIENOL)
   export DIY_WORK="${FOLDER_NAME}$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
 ;;
 IMMORTALWRT)
-  if [[ "${REPO_BRANCH}" == "mt798x" ]]; then
-    export REPO_URL="https://github.com/hanwckf/immortalwrt-mt798x"
-    export SOURCE="Immortalwrt"
-    export SOURCE_OWNER="hanwckf's"
-    export LUCI_EDITION="mt798x"
-    export DIY_WORK="hanwckf2102"
-    export REPO_BRANCH="openwrt-21.02"
-  else
-    export REPO_URL="https://github.com/immortalwrt/immortalwrt"
-    export SOURCE="Immortalwrt"
-    export SOURCE_OWNER="ctcgfw's"
-    export LUCI_EDITION="$(echo "${REPO_BRANCH}" |sed 's/openwrt-//g')"
-    export DIY_WORK="${FOLDER_NAME}$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
-  fi
+  export REPO_URL="https://github.com/immortalwrt/immortalwrt"
+  export SOURCE="Immortalwrt"
+  export SOURCE_OWNER="ctcgfw's"
+  export LUCI_EDITION="$(echo "${REPO_BRANCH}" |sed 's/openwrt-//g')"
+  export DIY_WORK="${FOLDER_NAME}$(echo "${LUCI_EDITION}" |sed "s/\.//g" |sed "s/\-//g")"
 ;;
 XWRT)
   export REPO_URL="https://github.com/x-wrt/x-wrt"
@@ -260,50 +251,6 @@ fi
 }
 
 
-function svn_co() {
-if [[ $# -lt 2 ]]; then
-  echo "格式错误,正确格式为: [svn_co] [文件夹或文件链接] [需要替换的文件夹或文件的对应路径],分别以空格分隔"
-  return 1
-fi
-A="$1" B="$2" && shift 2
-cd "${HOME_PATH}" && r="$PWD/"
-rootdir="$(echo "${B}"|sed "s?${r}??g"|sed 's/^.\///')"
-localdir="${HOME_PATH}/${rootdir}"
-fssl="$(echo "${A}" |cut -d"/" -f4-5)"
-curl="$(echo "${A}" |cut -d"/" -f1-5)"
-crutch="$(echo "${A}" |cut -d"/" -f6)"
-branch="$(echo "${A}" |cut -d"/" -f7)"
-test="$(echo "${A}" |cut -d"/" -f8-)"
-link="https://raw.githubusercontent.com/${fssl}/${branch}/${test}"
-if [[ "${crutch}" == "blob" ]]; then
-  curl -L "${link}" -o "${localdir}"
-  if [[ $? -ne 0 ]]; then
-    echo "${rootdir}文件下载失败,请检查网络,或查看链接正确性"
-    return 1
-  else
-    echo "${rootdir}文件替换成功"
-  fi
-elif [[ "${crutch}" == "tree" ]]; then
-  tmpdir="$(mktemp -d)" || exit 1
-  trap 'rm -rf "${tmpdir}"' EXIT
-  git clone -b "${branch}" --depth 1 --filter=blob:none --sparse "${curl}" "${tmpdir}"
-  cd "${tmpdir}"
-  git sparse-checkout init --cone
-  git sparse-checkout set "${test}"
-  if [[ $? -ne 0 ]]; then
-    echo "${rootdir}文件夹下载失败,请检查网络,或查看链接正确性"
-    return 1
-  else
-    echo "${rootdir}文件夹替换成功"
-  fi
-  sudo rm -rf "${localdir}" && cp -Rf "${test}" "${localdir}"
-  cd "${HOME_PATH}" && sudo rm -rf "${tmpdir}"
-else
-  echo "${rootdir}替换文件操作失败,请保证链接正确性"
-fi
-}
-
-
 function Diy_checkout() {
 # 下载源码后，进行源码微调和增加插件源
 cd ${HOME_PATH}
@@ -311,6 +258,11 @@ cd ${HOME_PATH}
 [[ ! -d "${HOME_PATH}/LICENSES/doc" ]] && mkdir -p "${HOME_PATH}/LICENSES/doc"
 [[ ! -d "${HOME_PATH}/build_logo" ]] && mkdir -p "${HOME_PATH}/build_logo"
 
+LUCI_CHECKUT="$(git tag -l |grep '^V\|^v' |awk 'END {print}')"
+if [[ -n "${LUCI_CHECKUT}" ]]; then
+  git checkout ${LUCI_CHECKUT}
+  git switch -c ${LUCI_CHECKUT}
+fi
 git pull
 
 sed -i '/cdny123/d; /helloworld/d; /passwall/d; /OpenClash/d' "feeds.conf.default"
